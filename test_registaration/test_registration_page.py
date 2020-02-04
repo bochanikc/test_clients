326bbf97b7c08ab6c6edb4450c6a9d60a73b88b7
nfrolov <nikitafrolovbnc@gmail.com>
2020-02-04 16:30:40 +0300

    Параметризированные тесты на ошибочный формат пароля

--- test_registaration/test_registration_page.py
+++ test_registaration/test_registration_page.py
@@ -23,7 +23,8 @@ class TestRegistrationPage:
     def test_registration_success(self, browser, test_go_to_registration_page, generate_test_data):
         bro = browser
         name, surname, email, company_name, password, confirm_password, phone = generate_test_data
-        RegistrationPage(bro).registration_new_user(name, surname, email, company_name, password, confirm_password, phone)
+        RegistrationPage(bro).registration_new_user(name, surname, email, company_name, password, confirm_password,
+                                                    phone)
         EmailSubmitPage(bro).located_email_submit_page()
         print('\n-------------------------------------------'
               + '\nemail: ' + email
@@ -67,4 +68,21 @@ class TestRegistrationPage:
         name, surname, email, company_name, password, confirm_password, phone = generate_test_data
         expected_fail_text = 'Поле пароль обязательно для заполнения.'
         RegistrationPage(bro).registration_new_user(name, surname, email, company_name, '', confirm_password, phone)
+        Alerts(bro).check_fail_alert_of_field(expected_fail_text)
+
+    @pytest.mark.parametrize('password_param, expected_fail_text',
+                             [
+                                 ('12345', 'Количество символов в поле пароль должно быть не менее 8.'),
+                                 ('Qwerty', 'Количество символов в поле пароль должно быть не менее 8.'),
+                                 ('Qwerty1', 'Количество символов в поле пароль должно быть не менее 8.'),
+                                 ('Qwertyui', 'Поле пароль имеет ошибочный формат.')
+                             ])
+    def test_incorrect_password_field(self, browser, test_go_to_registration_page, generate_test_data,
+                                      password_param, expected_fail_text):
+        bro = browser
+        name, surname, email, company_name, password, confirm_password, phone = generate_test_data
+        password = password_param
+        confirm_password = password_param
+        RegistrationPage(bro).registration_new_user(name, surname, email, company_name,
+                                                    password, confirm_password, phone)
         Alerts(bro).check_fail_alert_of_field(expected_fail_text)
