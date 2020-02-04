46c64e7e65f4915656470d9f683db56b3c9004dd
nfrolov <nikitafrolovbnc@gmail.com>
2020-02-04 14:11:22 +0300

    Добавил тесты на ошибки полей регистрации

--- test_registaration/test_registration_page.py
+++ test_registaration/test_registration_page.py
@@ -2,8 +2,6 @@ import random
 import time
 
 import pytest
-from selenium.webdriver.common.by import By
-from locators import EmailSubmit, Registration, Common
 from pages import RegistrationPage, EmailSubmitPage
 from pages.common import Alerts
 
@@ -17,15 +15,15 @@ def generate_test_data():
     company_name = "Company Selenium " + now_time + " inc."
     password = "Qwerty123"
     phone = random.randint(1000000000, 9999999999)
-    return name, surname, email, company_name, password, phone
+    return name, surname, email, company_name, password, password, phone
 
 
 class TestRegistrationPage:
 
     def test_registration_success(self, browser, test_go_to_registration_page, generate_test_data):
         bro = browser
-        name, surname, email, company_name, password, phone = generate_test_data
-        RegistrationPage(bro).registration_new_user(name, surname, email, company_name, password, phone)
+        name, surname, email, company_name, password, confirm_password, phone = generate_test_data
+        RegistrationPage(bro).registration_new_user(name, surname, email, company_name, password, confirm_password, phone)
         EmailSubmitPage(bro).located_email_submit_page()
         print('\n-------------------------------------------'
               + '\nemail: ' + email
@@ -38,7 +36,35 @@ class TestRegistrationPage:
 
     def test_empty_name_field(self, browser, test_go_to_registration_page, generate_test_data):
         bro = browser
-        name, surname, email, company_name, password, phone = generate_test_data
+        name, surname, email, company_name, password, confirm_password, phone = generate_test_data
         expected_fail_text = 'Поле имя обязательно для заполнения.'
-        RegistrationPage(bro).registration_new_user('', surname, email, company_name, password, phone)
+        RegistrationPage(bro).registration_new_user('', surname, email, company_name, password, confirm_password, phone)
+        Alerts(bro).check_fail_alert_of_field(expected_fail_text)
+
+    def test_empty_surname_field(self, browser, test_go_to_registration_page, generate_test_data):
+        bro = browser
+        name, surname, email, company_name, password, confirm_password, phone = generate_test_data
+        expected_fail_text = 'Поле фамилия обязательно для заполнения.'
+        RegistrationPage(bro).registration_new_user(name, '', email, company_name, password, confirm_password, phone)
+        Alerts(bro).check_fail_alert_of_field(expected_fail_text)
+
+    def test_empty_email_field(self, browser, test_go_to_registration_page, generate_test_data):
+        bro = browser
+        name, surname, email, company_name, password, confirm_password, phone = generate_test_data
+        expected_fail_text = 'Поле электронный адрес обязательно для заполнения.'
+        RegistrationPage(bro).registration_new_user(name, surname, '', company_name, password, confirm_password, phone)
+        Alerts(bro).check_fail_alert_of_field(expected_fail_text)
+
+    def test_empty_company_field(self, browser, test_go_to_registration_page, generate_test_data):
+        bro = browser
+        name, surname, email, company_name, password, confirm_password, phone = generate_test_data
+        expected_fail_text = 'Поле название компании обязательно для заполнения.'
+        RegistrationPage(bro).registration_new_user(name, surname, email, '', password, confirm_password, phone)
+        Alerts(bro).check_fail_alert_of_field(expected_fail_text)
+
+    def test_empty_password_field(self, browser, test_go_to_registration_page, generate_test_data):
+        bro = browser
+        name, surname, email, company_name, password, confirm_password, phone = generate_test_data
+        expected_fail_text = 'Поле пароль обязательно для заполнения.'
+        RegistrationPage(bro).registration_new_user(name, surname, email, company_name, '', confirm_password, phone)
         Alerts(bro).check_fail_alert_of_field(expected_fail_text)
