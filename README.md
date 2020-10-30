be0cc4354a8fd2178f4012175bb463933bc24b23
nfrolov <nikitafrolovbnc@gmail.com>
2020-10-30 16:39:56 +0300

    Change conftest and README file. Update .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-**Тесты для системы клиентов**
+**Tests for clients system**
+
+Selenium tests for browsers: _chrome, firefox, IE_.
+
+Project created with Page Object UI testing pattern.
+
+The project is built on py test + selenium and there are some small developments for connecting Allure
+
+Example of running tests:
+`pytest -s -v test_autorization_page.py --browser=ie`
+
+Parameters:
+ 
+ `--browser` `-B`:
+
+    Chrome: `chrome`
+
+    Firefox: `firefox`
+
+    IE: `ie`
+
+`--url` `-U`:
+    
+    URL for tests
--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ def pytest_addoption(parser):
     parser.addoption(
         "--browser", "-B",
         action="store",
-        default="chrome",
+        default=None,
         help="choose your browser"
     )
 
@@ -18,28 +18,23 @@ def pytest_addoption(parser):
     )
 
 
-# @pytest.fixture
-# def browser(request):
-#     browser_param = request.config.getoption("--browser")
-#     if browser_param == "chrome":
-#         driver = webdriver.Chrome()
-#     elif browser_param == "firefox":
-#         driver = webdriver.Firefox()
-#     elif browser_param == "ie":
-#         driver = webdriver.Ie()
-#     else:
-#         raise Exception(f"{browser_param} is not supported.")
-#
-#     driver.implicitly_wait(20)
-#     request.addfinalizer(driver.close)
-#     driver.get(request.config.getoption("--url"))
-#
-#     return driver
-
-
 @pytest.fixture(params=["chrome"])
 def browser(request):
-    browser_param = request.param
+    if request.config.getoption("--browser") is None:
+        browser_param = request.param
+    else:
+        browser_param = request.config.getoption("--browser")
+
+    driver = selection_browser(browser_param)
+
+    driver.implicitly_wait(5)
+    driver.set_window_size(1920, 1080)
+    driver.get(request.config.getoption("--url"))
+    request.addfinalizer(driver.quit)
+    return driver
+
+
+def selection_browser(browser_param):
     if browser_param == "chrome":
         driver = webdriver.Chrome()
     elif browser_param == "firefox":
@@ -50,11 +45,5 @@ def browser(request):
         driver = webdriver.Remote("http://172.16.169.137:4444/",
                                   desired_capabilities={'browserName': 'chrome'})
     else:
-        raise Exception(f"{request.param} is not supported!")
-
-    driver.implicitly_wait(5)
-    driver.set_window_size(1920, 1080)
-    driver.get(request.config.getoption("--url"))
-    request.addfinalizer(driver.quit)
-
+        raise Exception(f"{browser_param} is not supported!")
     return driver
